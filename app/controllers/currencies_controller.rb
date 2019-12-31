87d57879c9de4d35c4b43ff2666c55557fc068b5
Yair fernando <yair.facio11@gmail.com>
2019-12-30 21:14:19 -0600

    Finish search method of currencies controller

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -4,9 +4,11 @@ class CurrenciesController < ApplicationController
   end
 
   def search
-
+    @currencies = Currency.where('LOWER(name) LIKE ?', "%#{params[:search].downcase}%")
+    render json: { currencies: @currencies }
   end
 
+  # Takes the currency id and the amount owned, returns final calculations
   def calculate
 
   end
