c8e5bb55d93e1e478b2b56456007f1d7529f69ef
Yair fernando <yair.facio11@gmail.com>
2019-12-30 20:43:57 -0600

    Initial files and Currencies controller

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,13 @@
+class CurrenciesController < ApplicationController
+  def index
+
+  end
+
+  def search
+
+  end
+
+  def calculate
+
+  end
+end
---
+++ app/models/curency.rb
@@ -0,0 +1,2 @@
+class Curency < ApplicationRecord
+end
