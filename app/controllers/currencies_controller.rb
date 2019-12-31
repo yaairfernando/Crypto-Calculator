360935961a250d582dc0c0168c72197ff2ae9fe7
Yair fernando <yair.facio11@gmail.com>
2019-12-30 21:17:32 -0600

    Finish calculate method of currencies controller

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -10,6 +10,17 @@ class CurrenciesController < ApplicationController
 
   # Takes the currency id and the amount owned, returns final calculations
   def calculate
+    amount = params[:amount]
+    render json: {
+      currency: currency,
+      current_price: currency.current_price,
+      amount: amount,
+      value: currency.calculate_value(amount)
+    }
+  end
 
+  private
+  def currency
+    @currency ||= Currency.find(params[:id]) 
   end
 end
