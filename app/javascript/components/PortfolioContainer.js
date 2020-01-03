1d3d737df20cea0e0befb5a4ccca0441ddb6386f
Yair fernando <yair.facio11@gmail.com>
2020-01-03 10:44:23 -0600

    Getting the active currency

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -15,25 +15,31 @@ class PortfolioContainer extends Component {
       amount: ''
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
   }
   handleChange(e){
-    // this.setState({ [e.target.name]: e.target.value })
-    // console.log(this.state.name);
     axios.post('http://localhost:3000/search', {
       search: e.target.value
     })
     .then((data) => {
-      console.log(data)
+      this.setState({ search_results: [...data.data.currencies]})
     })
     .catch((data) => {
       debugger
     })
   }
 
+  handleSelect(e){
+    e.preventDefault()
+    const id = e.target.getAttribute('data-id')
+    const activeCurrency = this.state.search_results.filter(item => item.id == parseInt(id));
+    this.setState({ active_currency: activeCurrency[0], search_results: [] })
+  }
+
   render() {
     return(
       <div>
-        <Search handleChange={this.handleChange} />
+        <Search handleSelect={this.handleSelect} search_results={this.state.search_results} handleChange={this.handleChange} />
         <Calculate />
       </div>
     )
--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -6,6 +6,19 @@ class Search extends Component {
   }
 
   render() {
+    const search_results = this.props.search_results.map(currency => 
+      <li key={currency.id} data-id={currency.id} onClick={this.props.handleSelect} className="currency-list-item">
+        <a href="#" className="currency">
+          <span>
+            {currency.name}
+          </span> 
+          <span>
+            {currency.currency_symbol}
+          </span>
+        </a>
+      </li>
+    )
+
     return(
       <div>
         <h1>Cryptocurrency Calculator</h1>
@@ -16,6 +29,9 @@ class Search extends Component {
               autoComplete="off" type="text" name="name" placeholder="Search here...." 
               value={this.props.name} className="field" 
             />
+          </div>
+          <div className="currency-list">
+            {search_results}
           </div>
         </form>
       </div>
